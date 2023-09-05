76eee6783bc5ec8f3725e8dba43731b2b02e4860
ksb-dev <babaleshwarkedar@gmail.com>
2023-09-05 15:30:08 +0530

    initial commit

--- src/components/Mode/ModeIcon.tsx
+++ src/components/Mode/ModeIcon.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 
 // redux
 import { RootState } from "@/redux/store";
@@ -15,23 +15,9 @@ import styles from "./mode.module.css";
 const ModeIcon: React.FC = (): JSX.Element => {
   const mode = useSelector((state: RootState) => state.mode.mode);
   const dispatch = useDispatch();
-  const sunRef = useRef<HTMLSpanElement>(null)!;
-  const moonRef = useRef<HTMLSpanElement>(null)!;
-  const [state, setState] = useState(false);
-
-  useEffect(() => {
-    if (mode) {
-      moonRef.current!.style.transform = "translate(-50%, -50%) scale(1)";
-      sunRef.current!.style.transform = "translate(-50%, -50%) scale(0)";
-    } else {
-      sunRef.current!.style.transform = "translate(-50%, -50%) scale(1)";
-      moonRef.current!.style.transform = "translate(-50%, -50%) scale(0)";
-    }
-  }, [mode]);
 
   const handleMode = () => {
     dispatch(toggleMode());
-    setState(!state);
   };
 
   return (
@@ -43,12 +29,15 @@ const ModeIcon: React.FC = (): JSX.Element => {
           (mode ? " whiteBg2 blackColor1" : " blackBg2 whiteColor1")
         }
       >
-        <span ref={moonRef} className={styles.moon_icon}>
-          <PiMoonStarsLight />
-        </span>
-        <span ref={sunRef} className={styles.sun_icon}>
-          <PiSunLight />
-        </span>
+        {mode ? (
+          <span className={styles.moon_icon}>
+            <PiMoonStarsLight />
+          </span>
+        ) : (
+          <span className={styles.sun_icon}>
+            <PiSunLight />
+          </span>
+        )}
       </p>
     </div>
   );
