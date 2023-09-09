4fb25e4e5319f003f636765d3ccb5464bf788e03
ksb-dev <babaleshwarkedar@gmail.com>
2023-09-09 18:59:19 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['image.tmdb.org', 'upload.wikimedia.org']
-    }
+    },
+    experimental: {
+       serverActions: true,
+  },
 }
 
 module.exports = nextConfig
--- src/app/pages/wishlist/[mediaType]/page.tsx
+++ src/app/pages/wishlist/[mediaType]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useState, useEffect } from "react";
@@ -6,13 +7,30 @@ import { useSession } from "next-auth/react";
 // lib
 import { getWishlist } from "@/lib/getWishlist";
 
+// import { prisma } from "@/lib/prisma";
+// import { revalidatePath } from "next/cache";
+
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/lib/getAuthOptions";
+
+// async function getWishlist() {
+//   "use server";
+
+//   revalidatePath("http://localhost:3000/pages/wishlist/all");
+
+//   return await prisma.wishlist.findMany();
+// }
+
 export default function Wishlist() {
   const { data: session } = useSession();
   const [wishlist, setWishlist] = useState([]);
 
   useEffect(() => {
     session && getWishlist().then((res) => setWishlist(res));
-  }, [session]);
+  }, []);
+
+  //const session = await getServerSession(authOptions);
+  //const wishlist = session && (await getWishlist());
 
   return <div>{wishlist.length}</div>;
 }
