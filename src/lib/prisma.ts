fc51188603a0a1f58b734141667f0de834e6f6a8
ksb-dev <babaleshwarkedar@gmail.com>
2023-09-10 10:11:21 +0530

    initial commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,16 @@
-// import { PrismaClient } from "@prisma/client";
-// export const prisma = new PrismaClient();
-
 import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// import { PrismaClient } from "@prisma/client";
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ["query"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
