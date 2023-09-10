916a191280d49e1b1d487fdfd8857f5b7434f509
ksb-dev <babaleshwarkedar@gmail.com>
2023-09-10 10:41:54 +0530

    initial commit

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { ModeSlice } from "./services/getMode";
-import { MoviesSlice } from "./services/getMovies";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
     mode: ModeSlice.reducer,
-    movies: MoviesSlice.reducer,
   },
 });
 
