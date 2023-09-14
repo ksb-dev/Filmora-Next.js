bfa3549d4fa6f451870d0a1dd2a1b37f1345cff7
ksb-dev <babaleshwarkedar@gmail.com>
2023-09-14 10:43:27 +0530

    initial commit

--- src/components/Header/ProfileIcon.tsx
+++ src/components/Header/ProfileIcon.tsx
@@ -16,11 +16,13 @@ const ProfileIcon: React.FC = (): JSX.Element => {
   const profileModalRef = useRef<HTMLDivElement>(null);
 
   const showUserModal = () => {
+    profileRef.current!.style.borderBottom = "4px solid var(--blue)";
     profileModalRef.current!.style.transform = "scale(1)";
     profileModalRef.current!.style.opacity = "1";
   };
 
   const hideUserModal = () => {
+    profileRef.current!.style.borderBottom = "4px solid transparent";
     profileModalRef.current!.style.transform = "scale(0)";
     profileModalRef.current!.style.opacity = "0";
   };
